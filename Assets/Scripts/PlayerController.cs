7f5cd96685f6896ae4e7375109f5d40caf94c8d4
Sander <>
2023-11-18 01:29:12 +0100

    add first basic movement for player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    private float moveSpeed;
+    private float jumpForce;
+    private bool isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
+
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+
+        moveSpeed = 3.0f;
+        jumpForce = 3.0f;
+        isJumping = false;
+    }
+    
+    void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if(moveHorizontal != 0.0)
+        {
+            rb2D.AddForce(
+                new Vector2(moveHorizontal * moveSpeed, 0.0f),
+                ForceMode2D.Impulse
+            );
+        }
+
+        if (moveVertical != 0.0)
+        {
+            rb2D.AddForce(
+                new Vector2(0.0f, moveVertical * jumpForce),
+                ForceMode2D.Impulse
+            );
+        }
+    }
+}
