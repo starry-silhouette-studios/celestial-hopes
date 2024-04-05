b7234fecaf34571765223ea0529b710735f1a49c
Sander <markussander952@gmail.com>
2024-04-05 23:34:04 +0200

    wip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,6 +20,15 @@ public class PlayerController : MonoBehaviour
     {
         moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed;
 
+        if (moveHorizontal > 0.01f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveHorizontal < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
         if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
         {
             isJumping = true;
@@ -28,23 +37,13 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Move();
+        Vector2 movement = new Vector2(moveHorizontal, rb.velocity.y);
+        rb.velocity = movement;
 
         if (isJumping)
         {
-            Jump();
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = false;
         }
-    }
-
-    void Move()
-    {
-        Vector2 movement = new Vector2(moveHorizontal, rb.velocity.y);
-        rb.velocity = movement;
-    }
-
-    void Jump()
-    {
-        rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-        isJumping = false;
     }
 }
