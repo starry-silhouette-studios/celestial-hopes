454663737ad7bd64e36e824119cde3c2122faa87
chrisvdlee <chris.v.d.lee2004@gmail.com>
2024-06-27 21:14:24 +0200

    Added Healing

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -20,6 +20,12 @@ public class PlayerBehaviour : MonoBehaviour
         Debug.Log(GameManager.gameManager._playerHealth.Health);
     }
 
+    private void HealPlayer(int healing)
+    {
+        GameManager.gameManager._playerHealth.HealUnit(healing);
+        Debug.Log(GameManager.gameManager._playerHealth.Health);
+    }
+
     /// test
     private void PlayerTakeHeal(int healing)
     {
@@ -35,6 +41,16 @@ public class PlayerBehaviour : MonoBehaviour
             {
                 PlayerTakeDmg(enemy.damageAmount);
             }
+        }
+
+        if (collision.gameObject.CompareTag("Healing"))
+        {
+            HealingBehaviour health= collision.gameObject.GetComponent<HealingBehaviour>();
+            if (health != null)
+            {
+                HealPlayer(health.healing);
+            }
+            GameObject.Destroy(collision.gameObject);
         }
     }
 }
