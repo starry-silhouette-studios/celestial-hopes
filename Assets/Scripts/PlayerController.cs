11fefe33ea2a016d8dee6af1c48245797ef988eb
chrisvdlee <chris.v.d.lee2004@gmail.com>
2024-08-19 21:52:07 +0200

    Add walljump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,34 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
-    private Rigidbody2D rb;
     private float moveHorizontal;
+    
     private bool isJumping = false;
     private bool doubleJump = false;
-    
+
+    private bool isWallSliding;
+    private float wallSlidingSpeed = 2f;
+
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(10f, 20f);
+
     public Transform attackPoint;
-    public float attackRange = 0.5f;
     public LayerMask enemyLayers;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    public float attackRange = 0.5f;
     public int attackDamage = 10;
-    public Transform groundCheck;
+
     public float groundCheckRadius;
-    public LayerMask groundLayer;
     private bool isTouchingGround;
 
     private Animator animator;
@@ -26,7 +42,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
 
-        moveSpeed = 20.0f;
+        moveSpeed = 10.0f;
         jumpForce = 15.0f;
         
         animator.SetBool("isAttacking", false);
@@ -72,12 +88,19 @@ public class PlayerController : MonoBehaviour
         {
             Attack();
         }
+
+        WallSlide();
+        WallJump();
     }
 
     void FixedUpdate()
     {
-        Vector2 movement = new Vector2(moveHorizontal, rb.velocity.y);
-        rb.velocity = movement;
+        if (!isWallJumping)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, rb.velocity.y);
+            rb.velocity = movement;
+        }
+        
 
         //if (isJumping)
         //{
@@ -86,7 +109,74 @@ public class PlayerController : MonoBehaviour
         //    doubleJump = true;
         //}
     }
-    
+
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private void WallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -Mathf.Sign(transform.localScale.x);
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+
+            float horizontalVelocity = moveHorizontal * 0.5f;
+            float wallJumpVelocityX = wallJumpingDirection * wallJumpingPower.x + horizontalVelocity;
+
+            Debug.Log(horizontalVelocity);
+            
+            rb.velocity = new Vector2(wallJumpVelocityX, wallJumpingPower.y);
+
+            Invoke(nameof(DisableWallSlide), 0.1f);
+
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDirection)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    private void DisableWallSlide()
+    {
+        isWallSliding = false;
+    }
+
+    private void WallSlide()
+    {
+        if (IsWalled() && !isTouchingGround && moveHorizontal != 0f && !isWallJumping)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
     void Attack() 
     {
         Debug.Log(animator.GetBool("isAttacking"));
